5f587c444ad414982c2e320800dc6f279d94f869
benguimbis <benguimbis@live.fr>
2026-05-18 18:52:24 +0100

    Plot for q2b

---
+++ q2b.r
@@ -0,0 +1,165 @@
+
+library(dplyr)
+library(ggplot2)
+library(gganimate)
+library(tidyr)
+library(stringr)
+
+files <- file.path("data",list.files("data/")[-length(list.files("data/"))])
+
+df <- plyr::rbind.fill(Map(read.csv, files))
+
+stations <- read.csv(file.path("data", "stations.csv"))
+
+
+df1116 <- do.call(rbind, Map(read.csv, files[11:16]))
+
+df1718 <- do.call(rbind, Map(read.csv, files[17:18]))
+
+df0103 <- do.call(rbind, Map(read.csv, files[1:3]))
+
+df0410 <- do.call(rbind, Map(read.csv, files[4:10]))
+
+
+cols1 <- intersect(colnames(df0103), colnames(df0410))
+
+cols2 <- intersect(cols1, colnames(df1116))
+
+cols3 <- intersect(cols2, colnames(df1718))
+
+#df <- do.call(rbind, Map(\(x)x[cols3], list(df1718, df0103, df0410, df1116)))
+
+
+
+
+
+df <- inner_join(df, stations, by = join_by(station == id))
+
+df <- df |> transform(date = as.Date(date))|>
+  transform(year = as.numeric(format.Date(date, format = "%Y")),
+            month = factor(format.Date(date, format = "%m"), labels = month.abb)
+)
+
+
+
+df_monthly <- df |> filter(year >= 2008) |>group_by(year, month, station, name)|> 
+  summarise(across(c("BEN", "CO", "EBE", "MXY", "NMHC", "NO_2", "NOx", "OXY", 
+"O_3", "PM10", "PXY", "SO_2", "TCH", "TOL", "PM25", 
+"NO", "CH4"), ~sum(.x, na.rm = T)), .groups = "drop")|>
+  mutate(station2 = station,
+  date = lubridate::make_date(year = year, month = month, day = 1L)
+  )
+
+df_madrid <- df_monthly|>  group_by(date)|>
+  summarise(across(c("BEN", "CO", "EBE", "MXY", "NMHC", "NO_2", "NOx", "OXY", 
+"O_3", "PM10", "PXY", "SO_2", "TCH", "TOL", "PM25", 
+"NO", "CH4"), ~mean(.x, na.rm = T)), .groups = "drop")
+
+
+
+
+
+
+
+panel_plot <- function(df1, df2, x, y, name){
+
+  pollutant <- data.frame(
+  variable = c(
+    "SO_2", "CO", "NO", "NO_2", "PM25", "PM10", "NOx",
+    "O_3", "TOL", "BEN", "EBE", "MXY", "PXY", "OXY",
+    "TCH", "CH4", "NMHC"
+  ),
+  
+  description = c(
+    "sulphur dioxide",
+    "carbon monoxide",
+    "nitric oxide",
+    "nitrogen dioxide",
+    "particles smaller than 2.5 μm",
+    "particles smaller than 10 μm",
+    "nitrous oxides",
+    "ozone",
+    "toluene (methylbenzene)",
+    "benzene",
+    "ethylbenzene",
+    "m-xylene level",
+    "p-xylene",
+    "o-xylene",
+    "total hydrocarbons",
+    "methane level",
+    "non-methane hydrocarbons"
+  ),
+  
+  unit = c(
+    "μg/m³",
+    "mg/m³",
+    "μg/m³",
+    "μg/m³",    
+    "μg/m³",
+    "μg/m³",
+    "μg/m³",
+    "μg/m³",
+    "μg/m³",
+    "μg/m³",
+    "μg/m³",
+    "μg/m³",
+    "μg/m³",
+    "μg/m³",
+    "mg/m³",
+    "mg/m³",
+    "mg/m³"
+  )
+)
+
+
+  df1|> 
+  ggplot(aes(x = !!sym(x), y = !!sym(y)))+
+  geom_line(data = select(df1, - station), aes(group = station2), color = "grey80")+
+  geom_line(data = df2 , color = "#1f387b", linewidth = .5)+
+  geom_text(aes(x = max(date)-1000, y = max(!!sym(y))+5, label = str_trunc(name, 10) ), 
+  size = 3.5, color = "grey45", hjust = 0)+
+  geom_line(aes(group = station), color = "#933d22", linewidth = .8)+  
+  scale_x_date(limits =c(as.Date("2008-01-01"), as.Date("2018-04-01")) )+
+  labs(y = paste0("Monthly Emissions in ", y, " (", pollutant$unit[pollutant$variable == y], ")"))+
+    ggtitle(glue::glue("<b>Montly total air concentration of {pollutant$description[pollutant$variable == y]} in <span style = 'color:#933d22;'> areas of Madrid </span> compared to <span style = 'color:#1f387b;'> the median city emissions </span> </b><br>"))+
+  facet_wrap(vars(station), ncol = 5)+
+      theme_minimal(base_family =  "Lato", base_size = 14) +
+    theme(
+      axis.text.x = element_text(
+        size = 10,
+        color = "#111111",
+        face = "bold"
+      ),
+      legend.position = "top",
+      strip.text = element_blank(),
+      legend.title = element_blank(),
+      plot.background = element_rect(fill = "#FFF9F3", colour = NA),
+      panel.grid.minor = element_blank(),
+      panel.grid.major.y = element_blank(),
+      plot.title.position = "plot",
+      plot.title = ggtext::element_textbox_simple(face = "bold", family = "Playfair Display")
+    )
+}
+
+
+
+
+panel_plot(df_monthly, df_madrid, x = "date", y = "CO", name = "name")
+
+plots <- purrr::map(c(
+    "SO_2", "CO", "NO", "NO_2", "PM25", "PM10", "NOx",
+    "O_3", "TOL", "BEN", "EBE", "MXY", "PXY", "OXY",
+    "TCH", "CH4", "NMHC"),
+
+  ~panel_plot(df_monthly, df_madrid, x = "date", y = .x , name = "name")
+
+  ) |> setNames(nm = c(
+    "SO_2", "CO", "NO", "NO_2", "PM25", "PM10", "NOx",
+    "O_3", "TOL", "BEN", "EBE", "MXY", "PXY", "OXY",
+    "TCH", "CH4", "NMHC"))
+
+plots$O_3
+
+
+
+
